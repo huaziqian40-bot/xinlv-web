d573c8ad94f24da25745178d7991f96909ec0e5f
Developer <dev@moodsite.local>
2026-08-17 17:33:58 +0800

    下载页新增安卓 v1.2.31

--- core/views.py
+++ core/views.py
@@ -318,6 +318,7 @@ def disclaimer(request):
 def download(request):
     """下载页面：展示各平台客户端下载链接。"""
     android_versions = [
+        ("1.2.31", "download/android/xinlv-1.2.31.apk"),
         ("1.2.30", "download/android/xinlv-1.2.30.apk"),
         ("1.2.29", "download/android/xinlv-1.2.29.apk"),
         ("1.2.28", "download/android/xinlv-1.2.28.apk"),
